b9a149c369c68be4be2b60d849370656e222da7f
wodnjsrla85 <wodnjsrla85@naver.com>
2025-05-23 17:17:14 +0900

    test project

---
+++ test_app/lib/view/test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Test extends StatelessWidget {
+  const Test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+    //주석 놀이
+  }
+}
